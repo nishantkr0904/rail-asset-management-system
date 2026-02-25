97c77b3fa5e2138d33737c1c92c2ed380e9f8511
Nishant Kumar <nishantkr.0904@gmail.com>
2026-02-25 06:09:04 +0530

    chore: initialize Spring Boot project structure

---
+++ src/main/java/com/rail/asset/RailAssetManagementSystemApplication.java
@@ -0,0 +1,12 @@
+package com.rail.asset;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RailAssetManagementSystemApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(RailAssetManagementSystemApplication.class, args);
+    }
+}
---
+++ src/test/java/com/rail/asset/RailAssetManagementSystemApplicationTests.java
@@ -0,0 +1,13 @@
+package com.rail.asset;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RailAssetManagementSystemApplicationTests {
+
+    @Test
+    void contextLoads() {
+        // Ensures Spring context can start
+    }
+}
